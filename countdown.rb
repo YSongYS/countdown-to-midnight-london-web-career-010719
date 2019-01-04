9c530396359c92d5b368fdc0cd3f2080a46ed2b7
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 14:44:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
 def countdown (number)
+  number = number 
   while number > 0 do
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
